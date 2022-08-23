b253728b884beed079f606a90da3f3ee443d95e2
Jonas Ebert <jonasebert2016@hotmail.com>
2022-08-23 11:39:20 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jonas Ebert on 23.08.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
